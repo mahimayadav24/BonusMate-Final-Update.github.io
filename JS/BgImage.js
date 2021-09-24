2a8cd03260c0b11df57a8232bd54b9c3695d237b
mahimayadav24 <88049838+mahimayadav24@users.noreply.github.com>
2021-09-25 00:26:10 +0530

    insert

---
+++ JS/BgImage.js
@@ -0,0 +1,61 @@
+$("#button").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__one").css("backgroundImage", "url('../Assets/check (4) 4.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
+
+$("#btnsChk").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__Two").css("backgroundImage", "url('../Assets/check (4) 4.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
+
+$("#btnss4").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__Three").css("backgroundImage", "url('../Assets/check (4) 4.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
+
+
+$("#bnst1").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__Two").css("backgroundImage", "url('../Assets/check (4) 4.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
+$("#btnf1").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__Two").css("backgroundImage", "url('../Assets/v 1st.png')");
+    $("#Img__one").css("backgroundImage", "url('../Assets/Group 866.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
+$("#bnst1").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__Two").css("backgroundImage", "url('../Assets/v 1st.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
+$("#btnsg4").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__Three").css("backgroundImage", "url('../Assets/options (1) 1.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
+$("#btnf1").click(function () {
+    // ^ Attach a function on click event of an element with id="mylink"
+    //Change the css of a element with id="myDiv"
+    $("#Img__Three").css("backgroundImage", "url('../Assets/options (1) 1.png')");
+    // Note ^: In jQuery, the parameters with "-" as define that way
+    // But of course .css() supports "-" also, but make a habit of doing in camel-case
+});
